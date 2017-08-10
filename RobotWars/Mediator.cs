1ff6127935904844f41036c062ecbf9a85b59d35
Michael Moore <mikemoore356@gmail.com>
2017-08-10 09:55:49 +0100

    Initial Commit

---
+++ RobotWars/Mediator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RobotWars
+{
+    public class Mediator
+    {
+        public List<Robot> Robots { get; set; } = new List<Robot>();
+
+        public Mediator()
+        {
+            var robots = Assembly.GetEntryAssembly().DefinedTypes.Where(type => type.ImplementedInterfaces.Contains(typeof(IRobot))).ToList();
+            robots.ForEach(robot =>
+            {
+                var iRobot = (IRobot)Activator.CreateInstance(robot.AsType());
+                Robots.Add(new Robot
+                {
+                    Health = 100,
+                    RobotImplementation = iRobot,
+                    Id = Guid.NewGuid(),
+                    Name = iRobot.GetName(),
+                    LastTurn = DateTime.Now
+                });
+            });
+        }
+
+        public void NextTurn()
+        {
+            var robot = Robots.OrderBy(r => r.LastTurn).FirstOrDefault();
+            var competitors = Robots.Where(r => r.Health > 0 && r.Id != robot.Id).Select(r => new RobotAction
+            {
+                Health = r.Health,
+                Name = r.RobotImplementation.GetName(),
+                Attacks = 0,
+                Id = r.Id
+            }).ToList();
+            var attacks = new List<RobotAction>();
+            try
+            {
+                attacks = robot.RobotImplementation.MyTurn(competitors);
+                if(attacks.Sum(attack => attack.Attacks) > 10)
+                {
+                    // Tough luck, robot tried to cheat so forefits their turn
+                    attacks.Clear();
+                }
+            }
+            catch (Exception)
+            {
+                // Tough luck, there was an error so the robot forefits their turn.
+            }
+            attacks.ForEach(attack =>
+            {
+                var robotToAttack = Robots.SingleOrDefault(r => r.Id == attack.Id);
+                if(robotToAttack != null)
+                {
+                    robotToAttack.Health = robotToAttack.Health - attack.Attacks;
+                }
+                else
+                {
+                    // Tough luck, robot tried to manipulate the id's, cheating so firefits this attack
+                }
+            });
+
+            Robots.ForEach(r =>
+            {
+                r.RobotImplementation.UpdateHealth(r.Health);
+            });
+
+            robot.LastTurn = DateTime.Now;
+        }
+    }
+}
---
+++ RobotWarsConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using RobotWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RobotWarsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var robotsOut = new List<Guid>();
+            Console.ForegroundColor = ConsoleColor.White;
+            var war = new Mediator();
+            while(war.Robots.Count(robot => robot.Health > 0) > 1)
+            {
+                war.NextTurn();
+                war.Robots.OrderBy(robot => robot.Name).ToList().ForEach(robot =>
+                {
+                    if(robot.Health <= 0)
+                    {
+                        if(!robotsOut.Contains(robot.Id))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{robot.Name} is out! :(");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            robotsOut.Add(robot.Id);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{robot.Name} health is {robot.Health.ToString()}");
+                    }
+                });
+                Console.WriteLine("");
+                System.Threading.Thread.Sleep(500);
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{war.Robots.SingleOrDefault(r => r.Health > 0).Name} Wins! :)");
+            Console.ReadLine();
+        }
+    }
+}
