95e18214aefcf4d100559809be27c09eaafae39f
Michael Moore <michael.moore@timico.co.uk>
2018-07-10 16:18:57 +0100

    Added comments and fixed randomisation

--- RobotWars/Mediator.cs
+++ RobotWars/Mediator.cs
@@ -11,8 +11,11 @@ namespace RobotWars
         public List<Robot> Robots { get; set; } = new List<Robot>();
         public Int64 TurnCounter { get; set; } = 0;
 
+
         public Mediator(List<IRobot> robots)
         {
+
+            // Initialize the robots with 100 health each
             Robots = robots.Select(r => new Robot
             {
                 Health = 100,
@@ -21,16 +24,18 @@ namespace RobotWars
                 LastTurn = DateTime.Now,
                 Name = r.GetName()
             }).ToList();
-            Robots.ForEach(r =>
+
+            // Ensure there are no duplicate robots
+            if(Robots.Any(r => Robots.Count(b => b.Name == r.Name) > 1))
             {
-                if(Robots.Count(b => b.Name == r.Name) > 1)
-                {
-                    throw new Exception("Duplicate robots exist.");
-                }
-            });
+                throw new Exception("Duplicate robots exist.");
+            }
+
+            // Ensures that the starting order of the robots is randomised each time.
             RandomizeStartOrder();
         }
 
+        // Randomises the start order of the robots
         public void RandomizeStartOrder()
         {
             var tempRobots = Robots.Select(r => r).ToList();
@@ -49,12 +54,17 @@ namespace RobotWars
 
         public void NextTurn()
         {
-            if(TurnCounter == Robots.Count)
+            // If it is the first turn in a round, randomize the start order
+            if(TurnCounter % Robots.Count == 0)
             {
                 Robots.ForEach(r => { r.LastTurn = new DateTime(1900, 1, 1); });
                 RandomizeStartOrder();
             }
+
+            // Get the first robot whose health is greater than zero and it has been the longest since their last turn
             var robot = Robots.Where(r => r.Health > 0).OrderBy(r => r.LastTurn).FirstOrDefault();
+
+            // Get the robots competitors, these are those whose health is greater than zero and not itself
             var competitors = Robots.Where(r => r.Health > 0 && r.Id != robot.Id).Select(r => new RobotAction
             {
                 Health = r.Health,
@@ -62,6 +72,8 @@ namespace RobotWars
                 Attacks = 0,
                 Id = r.Id
             }).ToList();
+
+            // Generate a new list if attacks, and make attacks based on the bobots own implementation
             var attacks = new List<RobotAction>();
             try
             {
@@ -76,6 +88,8 @@ namespace RobotWars
             {
                 // Tough luck, there was an error so the robot forefits their turn.
             }
+
+            // Process each attack
             attacks.ForEach(attack =>
             {
                 var robotToAttack = Robots.SingleOrDefault(r => r.Id == attack.Id);
@@ -91,6 +105,7 @@ namespace RobotWars
 
             Robots.ForEach(r =>
             {
+                // Push the new health value to the implementation
                 r.RobotImplementation.UpdateHealth(r.Health);
             });
 
