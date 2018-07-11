01f8bc715f137d4ff2d3cf330fb36ef6e4188d2e
Michael Moore <michael.moore@timico.co.uk>
2018-07-11 11:04:47 +0100

    New robot

---
+++ RobotsStore/RoboCop.cs
@@ -0,0 +1,57 @@
+﻿using RobotWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RobotsStore.Robots
+{
+    public class RoboCop : IRobot
+    {
+        public Int64 Health { get; set; }
+
+        public string GetName()
+        {
+            return "RoboCop";
+        }
+
+        public List<RobotAction> MyTurn(List<RobotAction> competitors)
+        {
+            var robotsToIgnore = new List<String>
+            {
+                "Cheating Robot", "Very Stupid Robot", "Stupid Robot"
+            };
+            var attacks = 10;
+            var previousAttacks = attacks;
+            while (attacks > 0)
+            {
+                if (previousAttacks == attacks)
+                {
+                    competitors.ForEach(c =>
+                    {
+                        c.Attacks++;
+                        attacks--;
+                    });
+                }
+                else
+                {
+                    competitors.ForEach(c =>
+                    {
+                        if (!robotsToIgnore.Contains(c.Name))
+                        {
+                            c.Attacks++;
+                            attacks--;
+                        }
+                    });
+                }
+                previousAttacks = attacks;
+            }
+            return competitors;
+        }
+
+        public void UpdateHealth(Int64 health)
+        {
+            Health = health;
+        }
+    }
+}
